d7e42b233e81549baea1fe5ebad71bfdaa8c7e18
MAHA <mayouss30@gmail.com>
2022-05-12 21:41:20 +0100

    com

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -4,6 +4,7 @@ import {ActivatedRoute, Router} from "@angular/router";
 import {Component, OnInit} from "@angular/core";
 import {EntrepriseService} from "../services/entreprise.service";
 import {Entreprise} from "../Entreprise";
+import {AvisService} from "../services/avis.service";
 
 
 @Component({
@@ -15,14 +16,20 @@ export class SearchComponent implements OnInit {
   categorie!: string | null;
   nom!: string | null;
   entreprise!: Entreprise[];
+   aviss:any;
 
-  constructor(private entrepriseService:EntrepriseService, private activatedRoute: ActivatedRoute,private router:Router) {
+  constructor(private entrepriseService:EntrepriseService, private activatedRoute: ActivatedRoute,private router:Router,private avisservice:AvisService) {
   }
   ngOnInit(): void {
     this.entrepriseService.getAllVille(this.activatedRoute.snapshot.params["categorie"],this.activatedRoute.snapshot.params["ville"]).subscribe(prod => {
       this.entreprise = prod;
       console.log(prod)
     });
+    this.avisservice.count(this.entreprise).subscribe(prod => {
+      this.aviss = prod;
+
+    });
+
   }
   profile(societe: Entreprise) {
     this.router.navigate(['en/', societe.id]);
--- src/app/services/avis.service.ts
+++ src/app/services/avis.service.ts
@@ -8,8 +8,12 @@ import {avis} from "../avis";
 })
 export class AvisService {
   baseUrl='http://localhost:8082/api/v1/avis'
+  apiGett='http://localhost:8082/api/v1/countavis'
   constructor(private http: HttpClient) { }
   create(prod:avis): Observable<avis> {
     return this.http.post<avis>(this.baseUrl, prod);
   }
+  count(id:any | undefined):Observable<avis>{
+
+    const url = `${this.apiGett}/${id}`; return this.http.get<avis>(url); }
 }
